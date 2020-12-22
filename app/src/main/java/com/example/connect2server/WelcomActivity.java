9630f32b718d5caf1a8fd21db517e7dbbeb12450
范泽轩 <35322505+MicroFANs@users.noreply.github.com>
2020-12-22 15:12:39 +0800

    QMUITest
    创建QMUITest模块来使用一下

---
+++ app/src/main/java/com/example/connect2server/WelcomActivity.java
@@ -0,0 +1,14 @@
+package com.example.connect2server;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcom);
+    }
+}

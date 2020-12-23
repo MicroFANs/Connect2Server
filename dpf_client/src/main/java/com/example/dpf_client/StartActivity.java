e1edd77a918cb9ccb9fa73ecacf866dc6776f443
范泽轩 <35322505+MicroFANs@users.noreply.github.com>
2020-12-23 21:39:46 +0800

    完成start页面输入框

--- dpf_client/src/main/java/com/example/dpf_client/StartActivity.java
+++ dpf_client/src/main/java/com/example/dpf_client/StartActivity.java
@@ -2,69 +2,87 @@ package com.example.dpf_client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.InputType;
+import android.text.method.DigitsKeyListener;
 import android.view.View;
-
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.dd.CircularProgressButton;
 import com.dd.processbutton.iml.ActionProcessButton;
 
 
-public class StartActivity extends AppCompatActivity {
-    //ProgressButton
-    //ActionProcessButton mConnButton;
-    CircularProgressButton mConnectButton;
+public class StartActivity extends AppCompatActivity implements View.OnClickListener{
+    //控件
+
+    private CircularProgressButton mConnectBtn; //ProgressButton
+    private EditText mIPAddressEt;
+
+    private SharedPreferences mReadSP; //读取记录
+    private SharedPreferences.Editor mEditorSP; //保存记录
+
+
+    //变量
+    private String mIPAddress; //IP地址
+    private String mIPInput; //输入框中的字符串
+    private int mProgress; //ProgressButton进度数值
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         initView();  //初始化控件
-//        mConnButton.setMode(ActionProcessButton.Mode.ENDLESS);
-//        mConnButton.setProgress(0);
-//        mConnButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(mConnButton.getProgress()!=1){
-//                    mConnButton.setProgress(1);
-//                }
-//                else {
-//                    mConnButton.setProgress(-1);
-//                }
-//            }
-//        });
-
-        mConnectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CircularProgressButton btn=(CircularProgressButton) v;
-                int progress=btn.getProgress();
-                if (progress == 0) { // 初始时progress = 0
-                    btn.setProgress(50); // 点击后开始不精准进度，不精准进度的进度值一直为50
-                } else if (progress == 100) { // 如果当前进度为100，即完成状态，那么重新回到未完成的状态
-
-                    btn.setProgress(0);
-
-                } else if (progress == 50) { // 如果当前进度为50，那么点击后就显示完成的状态
-                    btn.setProgress(100);
-                }
-
-            }
-        });
 
     }
 
     //初始化控件
     private void initView() {
-        //初始化ProgressButton
+
         //mConnButton=findViewById(R.id.start_conn_btn);
-        mConnectButton=findViewById(R.id.start_progress_btn);
-        mConnectButton.setText("CONNECT");
-        mConnectButton.setCompleteText("Success");
-        mConnectButton.setErrorText("ERROR");
-        mConnectButton.setIdleText("CONNECT");
-        mConnectButton.setIndeterminateProgressMode(true);
+        // ProgressButton; -1:ERROR; 0:Idle; 100:SUCCESS
+        mConnectBtn=findViewById(R.id.start_btn_progress);
+        mConnectBtn.setText("CONNECT");
+        mConnectBtn.setCompleteText("SUCCESS");
+        mConnectBtn.setErrorText("ERROR");
+        mConnectBtn.setIdleText("CONNECT");
+        mConnectBtn.setIndeterminateProgressMode(true);
+        mConnectBtn.setOnClickListener(this);
+
+        //IP地址输入框
+        mIPAddressEt=findViewById(R.id.start_et_address);
+        mIPAddressEt.setInputType(InputType.TYPE_CLASS_NUMBER);
+        String digits = "0123456789.";
+        mIPAddressEt.setKeyListener(DigitsKeyListener.getInstance(digits)); //只能输入数字和.
+
+        //SharedPreferences读写记录
+        mReadSP =PreferenceManager.getDefaultSharedPreferences(this);
+
+        //读取上次保存的IP地址，填写到EditText
+        mIPAddress=mReadSP.getString("ipAddress","");
+        mIPAddressEt.setText(mIPAddress);
     }
 
+    //点击事件
+    @Override
+    public void onClick(View v) {
+        mIPInput=mIPAddressEt.getText().toString(); //获取输入字符串
+        mProgress=mConnectBtn.getProgress(); //进度
+        switch (v.getId()){
+            case R.id.start_btn_progress:
+                if(mIPInput.equals("")){
+                    Toast.makeText(this,"请输入IP地址",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    mIPAddress=mIPInput;
+                    mConnectBtn.setProgress(1);
+                    Toast.makeText(this,mIPAddress,Toast.LENGTH_SHORT).show();
+                }
 
+                break;
+        }
+    }
 }

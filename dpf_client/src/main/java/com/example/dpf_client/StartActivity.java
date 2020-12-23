a1894c28567694cf021af49a490db60940e0ef30
范泽轩 <35322505+MicroFANs@users.noreply.github.com>
2020-12-23 19:06:41 +0800

    ProgressButton搞好了
    不能直接按照晚上的弄，控件文字要用代码来设置，在xml文件里设置没用，圆角也没办法设置

--- dpf_client/src/main/java/com/example/dpf_client/StartActivity.java
+++ dpf_client/src/main/java/com/example/dpf_client/StartActivity.java
@@ -6,36 +6,64 @@ import android.os.Bundle;
 import android.view.View;
 
 
+import com.dd.CircularProgressButton;
 import com.dd.processbutton.iml.ActionProcessButton;
 
 
 public class StartActivity extends AppCompatActivity {
-    ActionProcessButton mConnButton;
+    //ProgressButton
+    //ActionProcessButton mConnButton;
+    CircularProgressButton mConnectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        initView();
-        mConnButton.setMode(ActionProcessButton.Mode.ENDLESS);
-        mConnButton.setProgress(0);
-        mConnButton.setOnClickListener(new View.OnClickListener() {
+        initView();  //初始化控件
+//        mConnButton.setMode(ActionProcessButton.Mode.ENDLESS);
+//        mConnButton.setProgress(0);
+//        mConnButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(mConnButton.getProgress()!=1){
+//                    mConnButton.setProgress(1);
+//                }
+//                else {
+//                    mConnButton.setProgress(-1);
+//                }
+//            }
+//        });
+
+        mConnectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mConnButton.getProgress()!=1){
-                    mConnButton.setProgress(1);
+                CircularProgressButton btn=(CircularProgressButton) v;
+                int progress=btn.getProgress();
+                if (progress == 0) { // 初始时progress = 0
+                    btn.setProgress(50); // 点击后开始不精准进度，不精准进度的进度值一直为50
+                } else if (progress == 100) { // 如果当前进度为100，即完成状态，那么重新回到未完成的状态
+
+                    btn.setProgress(0);
+
+                } else if (progress == 50) { // 如果当前进度为50，那么点击后就显示完成的状态
+                    btn.setProgress(100);
                 }
-                else {
-                    mConnButton.setProgress(-1);
-                }
+
             }
         });
 
     }
 
+    //初始化控件
     private void initView() {
-        mConnButton=findViewById(R.id.start_conn_btn);
-
+        //初始化ProgressButton
+        //mConnButton=findViewById(R.id.start_conn_btn);
+        mConnectButton=findViewById(R.id.start_progress_btn);
+        mConnectButton.setText("CONNECT");
+        mConnectButton.setCompleteText("Success");
+        mConnectButton.setErrorText("ERROR");
+        mConnectButton.setIdleText("CONNECT");
+        mConnectButton.setIndeterminateProgressMode(true);
     }
 
 
--- qmuitest/src/main/java/com/example/qmuitest/MainActivity.java
+++ qmuitest/src/main/java/com/example/qmuitest/MainActivity.java
@@ -8,22 +8,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ScrollView;
-import android.widget.SearchView;
+
 import android.widget.Toast;
 
+import com.dd.CircularProgressButton;
 import com.qmuiteam.qmui.widget.dialog.QMUIDialog;
 import com.qmuiteam.qmui.widget.dialog.QMUIDialogAction;
-import com.qmuiteam.qmui.widget.dialog.QMUIDialogBuilder;
 
-import scut.carson_ho.searchview.ICallBack;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     public static String TAG="TAG";
     Button mButton1;
     Button mButton2;
     Button mButton3;
-    SearchView mSearch;
+    CircularProgressButton cp;
+
 
 
     @Override
@@ -34,11 +33,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mButton1=findViewById(R.id.btn);
         mButton2=findViewById(R.id.qm_btn);
         mButton3=findViewById(R.id.qm_btn_1);
-        mSearch=(SearchView) findViewById(R.id.search_view);
+        cp=(CircularProgressButton) findViewById(R.id.processbtn);
+        cp.setText("链接");
+
+        cp.setCompleteText("Success");
+        cp.setErrorText("ERROR");
+        cp.setIdleText("链接");
+
 
         mButton1.setOnClickListener(this);
         mButton2.setOnClickListener(this);
         mButton3.setOnClickListener(this);
+
+        cp.setIndeterminateProgressMode(true);
+
+
+        cp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CircularProgressButton btn=(CircularProgressButton) v;
+                int progress=btn.getProgress();
+                if (progress == 0) { // 初始时progress = 0
+                    btn.setProgress(50); // 点击后开始不精准进度，不精准进度的进度值一直为50
+                } else if (progress == 100) { // 如果当前进度为100，即完成状态，那么重新回到未完成的状态
+
+                    btn.setProgress(0);
+
+                } else if (progress == 50) { // 如果当前进度为50，那么点击后就显示完成的状态
+                    btn.setProgress(100);
+                }
+
+            }
+        });
     }
 
     @Override
